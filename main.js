87dd3372340e141439fb5f9997226a86d504c8c2
Diego Souza <jeffdie@outlook.com>
2023-12-27 00:32:51 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,46 @@
+const form = document.getElementById('form-deposito')
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEhValido = false;
+
+function validaNome(nomeCompleto){
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length>= 2;
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemDeSucesso =  `Montante de: <b>${valorDeposito.value}</b>  depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>`
+    formEhValido = validaNome(nomeBeneficiario.value);
+
+    if (formEhValido){
+        const containerMensagemSucesso = document.querySelector('.success-message');
+        containerMensagemSucesso.innerHTML = mensagemDeSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+
+    } else{
+        nomeBeneficiario.style.border = '1px solid red'
+        document.querySelector('.error-message').style.display = 'block';
+        
+    }
+})
+
+nomeBeneficiario.addEventListener('keyup', function(e){
+    console.log(e);
+    formEhValido = validaNome(e.target.value);
+
+    if(!formEhValido){
+        nomeBeneficiario.classList.add('error');
+        document.querySelector('.error-message').style.display = 'block';
+        
+    } else{
+        nomeBeneficiario.classList.remove('error');
+        document.querySelector('.error-message').style.display = 'none';
+    }
+})
